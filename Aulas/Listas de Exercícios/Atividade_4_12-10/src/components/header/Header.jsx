d0c0b2051f99dcfb9eb0511654b9550fdffe1099
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-01-05 10:52:49 -0300

    fix: erro case-sensitive em paths e filenames. Observar nomes em letras maiúsculas e minúsculas. Em servidores os SOs são, na sua maioria, linux-like, portanto, case-sensitive.

--- Aulas/Listas de Exercícios/Atividade_4_12-10/src/components/header/Header.jsx
+++ Aulas/Listas de Exercícios/Atividade_4_12-10/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./header.css";
+import "./Header.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
--- Aulas/Listas de Exercícios/Atividade_4_12-10/src/pages/Home.jsx
+++ Aulas/Listas de Exercícios/Atividade_4_12-10/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/card/card";
+import Card from "../components/card/Card";
 import livros from "../data/livrosMock";
 import "../App.css";
 
